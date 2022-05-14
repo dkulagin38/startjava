01a22d03a44a968753c4c58a3bd9f920dbe8a9df
dkulagin38 <dkulagin38@gmail.com>
2022-05-14 08:27:21 +0800

    HW4.1 Bug fixing in the 'Calculator'.

--- src/com/startjava/lessons2_3_4/calculator/Calculator.java
+++ src/com/startjava/lessons2_3_4/calculator/Calculator.java
@@ -2,7 +2,34 @@ package com.startjava.lessons2_3_4.calculator;
 
 public class Calculator {
 
+/*
     public int calculate(String[] mathElements) {
+        int num1 = Integer.parseInt(mathElements[0]);
+        int num2 = Integer.parseInt(mathElements[2]);
+        char sign = mathElements[1].charAt(0);
+
+        switch (sign) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            case '%':
+                return num1 % num2;
+            case '^':
+                return (int) Math.pow(num1, num2);
+            default:
+                System.out.println("Введено некорректное значения арифметического знака.");
+                return 0;
+        }
+    }
+*/
+
+    public int calculate(String mathExpression) {
+        String[] mathElements = mathExpression.split(" ");
         int num1 = Integer.parseInt(mathElements[0]);
         int num2 = Integer.parseInt(mathElements[2]);
         char sign = mathElements[1].charAt(0);
--- src/com/startjava/lessons2_3_4/calculator/CalculatorMain.java
+++ src/com/startjava/lessons2_3_4/calculator/CalculatorMain.java
@@ -8,41 +8,20 @@ public class CalculatorMain {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        boolean askNext;
-
         do {
             Calculator calc = new Calculator();
             System.out.print("Введите математическое выражение: ");
             String mathExpression = scan.nextLine();
-            String[] mathElements = mathExpression.split(" ");
-
-            askNext = true;
-            if (mathElements.length != 3) {
-                System.out.println("Некорректное выражение. Количество элементов в выражении должно быть равно 3, " +
-                        "элементы разделены пробелами.");
-                askNext = false;
-                continue;
-            }
-
-            int result = calc.calculate(mathElements);
-/*
-            I don't use the value below because the result is "[2, ^, 10]" instead of "2 ^ 10".
-            String strMathElements = Arrays.toString(mathElements);
-            System.out.println(strMathElements + " = " + result);
-*/
-            System.out.println(mathElements[0] + " " + mathElements[1] + " " + mathElements[2] + " = " + result);
-
-        } while (isNext(askNext));
+            int result = calc.calculate(mathExpression);
+            System.out.println(mathExpression + " = " + result);
+        } while (isNext());
     }
 
-    private static boolean isNext(boolean askNext) {
-        if (!askNext) return true;
-
+    private static boolean isNext() {
         String continueMessage = "Хотите ввести новое выражение? [yes/no]: ";
         System.out.print(continueMessage);
 
         String answer;
-
         do {
             answer = scan.nextLine();
             if (isIncorrectAnswer(answer)) {
